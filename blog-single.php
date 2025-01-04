9ab1e960bf735845581ea23ca57e85145abdc3e6
Kenshin1903 <Gusdean20@gmail.com>
2025-01-05 04:28:36 +0700

    Doc

--- blog-single.php
+++ blog-single.php
@@ -56,7 +56,7 @@
         <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
           <div class="col-md-9 ftco-animate pb-5">
           	<p class="breadcrumbs"><span class="mr-2"><a href="index.php">Beranda<i class="ion-ios-arrow-forward"></i></a></span> <span class="mr-2"><a href="blog.php">Blog <i class="ion-ios-arrow-forward"></i></a></span> <span>Blog Single <i class="ion-ios-arrow-forward"></i></span></p>
-            <h1 class="mb-3 bread">Baca blog kami</h1>
+            <h1 class="mb-3 bread">Baca Blog Kami</h1>
           </div>
         </div>
       </div>
