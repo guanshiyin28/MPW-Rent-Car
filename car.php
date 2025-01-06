acecd59dd65c2517255cc6cabfda49f236b6f8b2
Rakakaka81 <rakaghalih25@gmail.com>
2025-01-06 21:56:20 +0700

    car.php Updates

--- car.php
+++ car.php
@@ -16,7 +16,7 @@ if ($conn->connect_error) {
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Carbook - Free Bootstrap 4 Template by Colorlib</title>
+    <title>Rental Mobil</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
