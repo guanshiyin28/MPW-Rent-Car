4d958ae35aa98a50e480c99906deb88cbd5114ea
Alik <alikzainul@gmail.com>
2025-01-07 22:01:06 +0700

    Add files via upload

--- data/sql/webrentalmobil.sql
+++ data/sql/webrentalmobil.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 04 Jan 2025 pada 22.09
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.2.12
+-- Generation Time: Jan 07, 2025 at 03:59 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `unit`
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `username`, `password`) VALUES
+(1, 'admin', '$2y$10$e0MYzXyjpJS7Pd0RVvHwHeFUP4P3cD.DJ8J5KqWj1gEQY4JnbE1i2'),
+(3, 'guan', '$2y$10$el8oBujLsCsEbfywLXMSC.urmlCbaJ.J8StazzsPRqiL/dG4BP9VK');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unit`
 --
 
 CREATE TABLE `unit` (
@@ -36,42 +56,54 @@ CREATE TABLE `unit` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `unit`
+-- Dumping data for table `unit`
 --
 
 INSERT INTO `unit` (`id`, `nama_mobil`, `harga_mobil`, `unit_mobil`, `gambar_mobil`) VALUES
-(1, 'Fortuner', 500000.00, 5, 'images/car-1.jpg'),
-(2, 'Avanza Veloz', 400000.00, 3, 'images/car-2.jpg'),
-(3, 'Civic', 1000000.00, 7, 'images/car-3.jpg'),
-(4, 'Pajero', 500000.00, 4, 'images/car-4.jpg'),
-(5, 'Sigra', 400000.00, 6, 'images/car-5.jpg'),
-(6, 'Fortuner Vrz', 500000.00, 2, 'images/car-6.jpg'),
-(7, 'Rush', 500000.00, 10, 'images/car-7.jpg'),
-(8, 'Rubicon', 1000000.00, 5, 'images/car-8.jpg'),
-(9, 'Range Rover', 1500000.00, 4, 'images/car-9.jpg'),
-(10, 'Xenia', 400000.00, 8, 'images/car-10.jpg'),
-(11, 'Avanza Veloz', 450000.00, 6, 'images/car-11.jpg'),
-(12, 'Crv', 500000.00, 5, 'images/car-12.jpg');
+(1, 'Fortuner', 500000.00, 4, 'assets/images/car/Fortuner.jpg'),
+(2, 'Avanza Veloz', 400000.00, 3, 'assets/images/car/Avanza.jpg'),
+(3, 'Civic', 1000000.00, 7, 'assets/images/car/Civic.jpg'),
+(4, 'Pajero', 500000.00, 4, 'assets/images/car/Pajero.jpg'),
+(5, 'Sigra', 400000.00, 6, 'assets/images/car/Sigra.jpg'),
+(7, 'Rush', 500000.00, 10, 'assets/images/car/Rush.jpg'),
+(8, 'Rubicon', 1000000.00, 5, 'assets/images/car/Rubicon.jpg'),
+(9, 'Range Rover', 1500000.00, 4, 'assets/images/car/RangeRover.jpg'),
+(10, 'Xenia', 400000.00, 8, 'assets/images/car/Xenia.jpg'),
+(12, 'Mercedes', 500000.00, 5, 'assets/images/car/Mercedes.jpg'),
+(50, 'Mazda RX-7', 9999.99, 1, 'assets/images/car/MazdaRX-7.jpg');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `unit`
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `unit`
 --
 ALTER TABLE `unit`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `unit`
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `unit`
 --
 ALTER TABLE `unit`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
